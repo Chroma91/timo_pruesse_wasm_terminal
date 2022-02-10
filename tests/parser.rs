b09765736cc02061d12a9e715f5dd99c0e076f37
Timo Prüße <chroma91@gmail.com>
2022-02-10 14:34:26 +0100

    add test for timo command

--- tests/parser.rs
+++ tests/parser.rs
@@ -6,12 +6,23 @@ use timo_pruesse_wasm_terminal::parser::*;
 
 #[test]
 fn test_parse_command() {
-    let timo_command = parse_command("timo --version").unwrap();
+    let timo_version_command = parse_command("timo --version").unwrap();
 
-    assert_eq!(timo_command.run().unwrap(), get_version());
+    assert_eq!(timo_version_command.run().unwrap(), get_version());
 }
 
 #[test]
 fn test_parse_invalid_command() {
     assert!(parse_command("failing command").is_err());
+}
+
+#[test]
+fn test_parse_timo_command() {
+    let timo_command = parse_command("timo").unwrap();
+    let timo_help_command = parse_command("timo --help").unwrap();
+
+    assert_eq!(
+        timo_command.run().unwrap(),
+        timo_help_command.run().unwrap()
+    );
 }
