0e6f5ae2975f88281f9346a621721defeb251b11
Timo Prüße <chroma91@gmail.com>
2022-04-04 18:35:50 +0200

    update system info

--- src/commands/timo_system.rs
+++ src/commands/timo_system.rs
@@ -12,10 +12,10 @@ impl TerminalCommand for TimoSystemCommand {
         <div style="margin:0.5rem 0 1rem 2rem">
             <table class="info-table">
                 <tr><th colspan="2">"WSL 2"</th></tr>
-                <tr><td style="padding-top:0.5rem">"Distributor ID"</td><td style="padding-top:0.5rem">"Debian"</td></tr>
-                <tr><td>"Description"</td><td>"Pengwin"</td></tr>
-                <tr><td>"Release"</td><td>"11"</td></tr>
-                <tr><td>"Codename"</td><td>"bullseye"</td></tr>
+                <tr><td style="padding-top:0.5rem">"Distributor ID"</td><td style="padding-top:0.5rem">"Ubuntu"</td></tr>
+                <tr><td>"Description"</td><td>"Ubuntu 20.04.4 LTS"</td></tr>
+                <tr><td>"Release"</td><td>"20.04"</td></tr>
+                <tr><td>"Codename"</td><td>"focal"</td></tr>
             </table>
         </div>
         );
